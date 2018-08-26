63aee6fd792e25e1266f791314d8259a14c3159a
MiglyA <33301907+MiglyA@users.noreply.github.com>
2018-08-26 16:53:17 +0900

    Update RioneLauncher_migration2.sh

--- RioneLauncher_migration2.sh
+++ RioneLauncher_migration2.sh
@@ -3,9 +3,9 @@
 #バージョン移行専用
 
 echo
-echo "バージョンを4.10に移行します..."
+echo "バージョンを5.10に移行します..."
 rm $0
-wget -N -O RioneLauncher.sh `curl https://raw.githubusercontent.com/MiglyA/bash-rescue/master/histry.txt | grep "RioneLauncher3-link" | awk '{print $2}'` &>/dev/null
+wget -N -O RioneLauncher.sh `curl https://raw.githubusercontent.com/Ri--one/bash-rescue/master/histry.txt | grep "RioneLauncher4-link" | awk '{print $2}'` &>/dev/null
 echo
 echo
 echo "再起動をお願いします。"
